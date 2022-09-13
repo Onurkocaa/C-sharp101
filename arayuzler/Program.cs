d1b927820d69dcf968ce5ee9746e6f4eb5bf0261
Onurkocaa <onurkoca.1347@gmail.com>
2022-09-13 20:35:51 +0300

    -

---
+++ arayuzler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arayuzler
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+         FileLogger filelogger = new();
+         filelogger.WriteLog();
+
+         DatabaseLogger databaselogger = new();
+         databaselogger.WriteLog();
+
+         SmsLogger smslogger = new();
+         smslogger.WriteLog();
+
+         LogManager logManager = new LogManager(new FileLogger());
+         logManager.WriteLog();
+       }
+    }
+}
